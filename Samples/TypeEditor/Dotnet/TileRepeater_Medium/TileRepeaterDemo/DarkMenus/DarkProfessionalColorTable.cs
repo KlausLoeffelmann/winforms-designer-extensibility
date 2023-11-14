47458e594e29762d660ccb752cf052398cabeac2
Klaus Loeffelmann <klaus.loeffelmann@microsoft.com>
2023-11-13 16:04:44 -0800

    Bump up versions to .NET 8 (Rc2-based).

--- Samples/TypeEditor/Dotnet/TileRepeater_Medium/TileRepeaterDemo/DarkMenus/DarkProfessionalColorTable.cs
+++ Samples/TypeEditor/Dotnet/TileRepeater_Medium/TileRepeaterDemo/DarkMenus/DarkProfessionalColorTable.cs
@@ -1,7 +1,4 @@
-﻿using System.Drawing;
-using System.Windows.Forms;
-
-namespace TileRepeaterDemo
+﻿namespace TileRepeaterDemo
 {
     internal class DarkProfessionalColorTable : ProfessionalColorTable
     {
